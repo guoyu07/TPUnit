ba91804c6662421f4cd3eebaddee55ff3cf21d11
Jimmy Jaw <web3d@live.com>
2016-05-20 10:52:54 +0800

    最新NB版本已经能自动生TP格式的成测试类，但文件名没有与类名对应，fix

--- src/TimeCheer/Test/Loader.class.php
+++ src/TimeCheer/Test/Loader.class.php
@@ -15,29 +15,46 @@ namespace TimeCheer\Test;
 class Loader
 {
 
-    private $dir;
+    protected $dir;
+
+    protected $prefix = 'UnitTest\\';
+    
+    /**
+     *
+     * @var string TP的坑爹文件扩展名造成的设定
+     */
+    protected $suffix = '.classTest';
 
     const PREFIX = 'TimeCheer\OAO\UnitTest\\';
 
     /**
      * 构造函数
      * @param string $dir 手动指定起始目录
+     * @param string $prefix 指定要加载的测试类命名空间前缀
+     * @param string $suffix NB生成的测试文件后缀名 一般不用指定
      */
-    public function __construct($dir)
+    public function __construct($dir, $prefix = '', $suffix = '')
     {
         $this->dir = $dir;
+        if ($prefix) {
+            $this->prefix = $prefix;
+        }
+        if ($suffix) {
+            $this->suffix = $suffix;
+        }
     }
 
     /**
      * 向PHP注册SPL autoloader
-     * @param string $dir 指定加载目录
+     * @param string $dir 指定要加载的测试类所在目录
+     * @param string $prefix 指定要加载的测试类命名空间前缀
      * @return void
      */
-    public static function register($dir)
+    public static function register($dir, $prefix = '', $suffix = '')
     {
         ini_set('unserialize_callback_func', 'spl_autoload_call');
 
-        spl_autoload_register(array(new self($dir), 'autoload'), FALSE, TRUE);
+        spl_autoload_register(array(new self($dir, $prefix, $suffix), 'autoload'), FALSE, TRUE);
     }
 
     /**
@@ -53,7 +70,7 @@ class Loader
             return false;
         }
 
-        if (file_exists($file = $this->dir . '/' . str_replace('\\', '/', str_replace(self::PREFIX, '', $class)) . '.php')) {
+        if (file_exists($file = $this->dir . '/' . substr(str_replace('\\', '/', str_replace($this->prefix, '', $class)), -1, 5) . $this->suffix . '.php')) {
             require_once $file;
 
             return true;
